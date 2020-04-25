07a13ffd2730f66171f27ada50b0da295571e29d
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-25 08:36:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,8 @@ class Song
   
   def self.new_from_filename(name)
     song = self.new
-    song.name = name.split("-").[1].
+    song.name = name.split("-")[1].chomp(".mp3")
+    song
   end
   
   def save
